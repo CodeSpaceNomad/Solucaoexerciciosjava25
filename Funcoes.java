ecfa8970b2d722e2365221e55830f02ad71af7fe
Raul Cortepasse Filho <r.cortepasse@edu.pucrs.br>
2025-08-05 10:04:44 -0300

    Add files via upload

---
+++ Funcoes.java
@@ -0,0 +1,55 @@
+
+public class Funcoes {
+    
+    public int fa(int n) {
+        int cont=0;
+
+        return cont;
+    }
+
+    public int fb(int n) {
+        int cont=0;
+
+        return cont;
+    }
+
+    public int fc(int n) {
+        int cont=0;
+
+        return cont;
+    }
+
+    public int fd(int n) {
+        int cont=0;
+
+        return cont;
+    }
+
+    public int fe(int n) {
+        int cont=0;
+
+        return cont;
+    }
+
+    public int ff(int n) {
+        int cont=0;
+
+        return cont;
+    }
+
+    public int fg(int n) {
+        int cont=0;
+
+        return cont;
+    }
+
+    public int fh(int n) {
+        int cont=0;
+
+        return cont;
+    }    
+    
+    public int getTotalSequencias(int v) {
+        return 0;
+    }
+}
