fa15d2268385ab553df41ec93a49add331d24144
Raul Cortepasse Filho <r.cortepasse@edu.pucrs.br>
2025-08-10 21:37:43 +0000

    Save03

--- diasemanaver2/src/main/java/com/bcopstein/App.java
+++ diasemanaver2/src/main/java/com/bcopstein/App.java
@@ -2,11 +2,13 @@ package com.bcopstein;
 
 public class App {
     public static void main(String[] args) {
+        boolean dataes = true;
         int diasema = 1;
 
         if (diasema < 1 || diasema > 7) {
             throw new IllegalArgumentException("Número inválido, digite um número de 1 até 7");
         }
+        if (dataes == false){ 
 
         switch (diasema) {
             case 1 -> System.out.println("Domingo");
@@ -17,10 +19,16 @@ public class App {
             case 6 -> System.out.println("Sexta-feira");
             case 7 -> System.out.println("Sábado");
         }
+        } else {
+            switch (diasema) {
+            case 1 -> System.out.println("Dia do senhor");
+            case 2 -> System.out.println("Segunda-feira");
+            case 3 -> System.out.println("Terça-feira");
+            case 4 -> System.out.println("Quarta-feira");
+            case 5 -> System.out.println("Quinta-feira");
+            case 6 -> System.out.println("Sexta-feira");
+            case 7 -> System.out.println("Dia de descanso");
+        }           
+        }
     }
 }
-        
-        
-    
-
-    
